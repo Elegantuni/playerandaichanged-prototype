3a877eb2e60050d11074b05755946ba7268f9032
Anthony Brown <groundup2360917182914017@gmail.com>
2018-08-10 16:48:55 -0400

    Update cpp porting

--- main.cpp
+++ main.cpp
@@ -59,7 +59,7 @@ typedef SSIZE_T ssize_t;
 
 #include "videoprinterarg2.h"
 
-void videoprinterstats(int y, int x, const char* m1, int a1, char* b1, int c1, int d1, int e1, char* f1, int g1, int h1, char* i1, char* j1, char* k1, int l1);
+#include "videoprinterstats.h"
 
 #include "magic.h"
 
@@ -3388,11 +3388,4 @@ beginning:
 	
 		return 0;
 	}
-}
-
-void videoprinterstats(int y, int x, const char* m1, int a1, char* b1, int c1, int d1, int e1, char* f1, int g1, int h1, char* i1, char* j1, char* k1, int l1)
-{
-	#ifdef INITNCURSESNOW
-	ncursesprintstats(y, x, m1, a1, b1, c1, d1, e1, f1, g1, h1, i1, j1, k1, l1);
-	#endif
 }
