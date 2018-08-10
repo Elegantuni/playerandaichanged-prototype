96fa58485b50d6312fdae8e81f18dceff4b807fc
Anthony Brown <groundup2360917182914017@gmail.com>
2018-08-10 16:22:02 -0400

    Update cpp porting

--- main.cpp
+++ main.cpp
@@ -51,7 +51,8 @@ typedef SSIZE_T ssize_t;
 
 #include "ncursesprintarg2.h"
 
-void ncursesprintstats(int y, int x, const char* m1, int a1, char* b1, int c1, int d1, int e1, char* f1, int g1, int h1, char* i1, char* j1, char* k1, int l1);
+#include "ncursesprintstats.h"
+
 void videoprinternorm(int y, int x, const char* c);
 void videoprinterarg1(int y, int x, const char* c, int d);
 void videoprinterarg2(int y, int x, const char* c, int d, char* e);
@@ -3384,11 +3385,6 @@ beginning:
 	
 		return 0;
 	}
-}
-
-void ncursesprintstats(int y, int x, const char* m1, int a1, char* b1, int c1, int d1, int e1, char* f1, int g1, int h1, char* i1, char* j1, char* k1, int l1)
-{
-	mvprintw(y, x, m1, a1, b1, c1, d1, e1, f1, g1, h1, i1, j1, k1, l1);
 }
 
 void videoprinternorm(int y, int x, const char *c)
---
+++ ncursesprintstats.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#ifndef INITWINDOWSNOW
+#include <ncurses.h>
+#else
+#include <curses.h>
+#endif
+
+void ncursesprintstats(int y, int x, const char* m1, int a1, char* b1, int c1, int d1, int e1, char* f1, int g1, int h1, char* i1, char* j1, char* k1, int l1);
