bf4346d99bd8d2ca4c9a19ef462d99cb4aeb8733
Anthony Brown <groundup2360917182914017@gmail.com>
2018-08-10 15:59:51 -0400

    Update cpp porting

--- main.cpp
+++ main.cpp
@@ -45,7 +45,8 @@ typedef SSIZE_T ssize_t;
 
 #include "inputgetter.h"
 
-void ncursesprint(int y, int x, const char* c);
+#include "ncursesprint.h"
+
 void ncursesprintarg1(int y, int x, const char* c, int d);
 void ncursesprintarg2(int y, int x, const char* c, int d, char* e);
 void ncursesprintstats(int y, int x, const char* m1, int a1, char* b1, int c1, int d1, int e1, char* f1, int g1, int h1, char* i1, char* j1, char* k1, int l1);
@@ -3381,11 +3382,6 @@ beginning:
 	
 		return 0;
 	}
-}
-
-void ncursesprint(int y, int x, const char* c)
-{
-	mvprintw(y, x, c);
 }
 
 void ncursesprintarg1(int y, int x, const char* c, int d)
